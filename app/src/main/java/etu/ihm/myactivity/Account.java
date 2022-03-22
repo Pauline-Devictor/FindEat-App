343c97e65e7ba6474fda29f1373d94311b45590f
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2022-03-22 23:52:22 +0100

    Pire commit de ma vie

--- app/src/main/java/etu/ihm/myactivity/Account.java
+++ app/src/main/java/etu/ihm/myactivity/Account.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Account extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account);
         (findViewById(R.id.navAccount)).setOnClickListener(click -> {
             Intent intentSend = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(intentSend);
