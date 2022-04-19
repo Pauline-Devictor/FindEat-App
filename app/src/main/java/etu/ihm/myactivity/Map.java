21897c95c85d1c038f6aa851f5338eb2e8db3bc3
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2022-04-19 23:46:36 +0200

    map spaws vraiment sur ping user

--- app/src/main/java/etu/ihm/myactivity/Map.java
+++ app/src/main/java/etu/ihm/myactivity/Map.java
@@ -1,6 +1,7 @@
 package etu.ihm.myactivity;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,6 +9,7 @@ import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
@@ -19,6 +21,9 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -45,15 +50,16 @@ public class Map extends AppCompatActivity {
     private final String TAG = "polytech-" + getClass().getSimpleName();
     private MapView map;
     private IMapController mapController; //gere les options de la map == zoom et centre au lancement
-    private double userlatitude=0;
-    private double userlongitude=0;
+    private double userlatitude = 0;
+    private double userlongitude = 0;
     private FusedLocationProviderClient fusedLocationProviderClient;
-
+    private LocationCallback locationCallback;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "starting map activity");
         super.onCreate(savedInstanceState);
         Configuration.getInstance().load(
                 getApplicationContext(),
@@ -97,21 +103,21 @@ public class Map extends AppCompatActivity {
 
 
         //creation d'un element
-        OverlayItem ralloOffice = new OverlayItem("Rallo's Office","His office",new GeoPoint(43.65020,7.00517));//Titre, Sous-titre, Position géographique
+        OverlayItem ralloOffice = new OverlayItem("Rallo's Office", "His office", new GeoPoint(43.65020, 7.00517));//Titre, Sous-titre, Position géographique
         Drawable m = ralloOffice.getMarker(0); //TODO sert à changer la forme du marqueur
         ArrayList<OverlayItem> items = new ArrayList<>(); //Liste d'element qu'on pourra afficher sur la carte
         items.add(ralloOffice);
-        items.add(new OverlayItem("Resto","chez babar",new GeoPoint(43.64950,7.00517)));
+        items.add(new OverlayItem("Resto", "chez babar", new GeoPoint(43.64950, 7.00517)));
 
         //Objet contenant la liste des elements ainsi que les actions auxquels ils reagissent -> ici TapUp() et LongPress()
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(),items,new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>(){
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
             @Override
-            public boolean onItemSingleTapUp(int index, OverlayItem item){
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
                 return true;
             }
 
             @Override
-            public boolean onItemLongPress(int index, OverlayItem item){
+            public boolean onItemLongPress(int index, OverlayItem item) {
                 return false;
             }
         });
@@ -121,17 +127,38 @@ public class Map extends AppCompatActivity {
     }
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             getLastLocation();
-        }
-        else {
+        } else {
             askLastLocationPermission();
         }
     }
 
-    public void getLastLocation(){
+    public void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 10001);
+            return;
+        }
+
+        Log.d(TAG,"locationcallback");
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    Log.d(TAG,"locationresult null");
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    Log.d(TAG,"lat "+location.getLatitude()+" long "+location.getLongitude());
+                }
+            }
+        };
+
+        fusedLocationProviderClient.requestLocationUpdates(new LocationRequest(), locationCallback, Looper.getMainLooper());
+
+
         Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
         locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
@@ -143,6 +170,7 @@ public class Map extends AppCompatActivity {
                     GeoPoint userPing = new GeoPoint(userlatitude,userlongitude);
                     mapController.setCenter(userPing);
                     OverlayItem overlayPing = new OverlayItem("me","ping",userPing);
+                    overlayPing.setMarker(getApplicationContext().getResources().getDrawable(R.drawable.ic_userping));
                     ArrayList<OverlayItem> items = new ArrayList<>();
                     items.add(overlayPing);
                     ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(),items,new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>(){
