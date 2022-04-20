f56580f2b6a0cfc1605448c74f4ec71c4114a613
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2022-04-20 13:01:00 +0200

    Classe restaurant

---
+++ app/src/main/java/etu/ihm/myactivity/FiltreEnum.java
@@ -0,0 +1,5 @@
+package etu.ihm.myactivity;
+
+public enum FiltreEnum {
+    VEGAN, VEGETARIEN, HALAL, CASHER
+}
---
+++ app/src/main/java/etu/ihm/myactivity/Restaurant.java
@@ -0,0 +1,11 @@
+package etu.ihm.myactivity;
+
+import android.graphics.Bitmap;
+
+public class Restaurant {
+    private Bitmap picture;
+    private String name;
+    private String description;
+    private float grade;
+    private String[] comments;
+}
