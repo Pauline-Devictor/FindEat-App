fb53a945e938a1acc6ca715a801d1d55e98c0b97
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-04-26 20:44:06 +0200

    Factory method

--- app/src/main/java/etu/ihm/myactivity/factoryTests/LieuxFactory.java
+++ app/src/main/java/etu/ihm/myactivity/factoryTests/LieuxFactory.java
@@ -6,12 +6,11 @@ import java.util.ArrayList;
 
 import etu.ihm.myactivity.restaurants.Commentaire;
 
-public class LieuxFactory {
-    public static final int RESTAURANT=1;
-    public static final int BAR=2;
+public class LieuxFactory extends RestaurationFactory {
 
-    public static Lieux build(String name, int type, Bitmap picture, String description,
-                              double grade, ArrayList<Commentaire>comments) throws Throwable{
+
+    public Lieux build(String name, int type, Bitmap picture, String description,
+                       double grade, ArrayList<Commentaire> comments) throws Throwable{
         switch (type){
             case RESTAURANT: return new Restaurants(name,picture,description,grade,comments);
             case BAR: return new Bar(name,picture,description,grade,comments);
@@ -19,5 +18,4 @@ public class LieuxFactory {
 
         }
     }
-
 }
