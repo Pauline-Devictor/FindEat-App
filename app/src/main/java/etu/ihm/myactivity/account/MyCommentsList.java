4142b2f1e1448bfd4d0915b630ed2b7102f54bd2
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2022-05-11 14:16:09 +0200

    modifs design

--- app/src/main/java/etu/ihm/myactivity/account/MyCommentsList.java
+++ app/src/main/java/etu/ihm/myactivity/account/MyCommentsList.java
@@ -10,7 +10,7 @@ public class MyCommentsList {
         commentList = new ArrayList<MyComments>();
         commentList.add(new MyComments("Chez Jean","4/5","29/09","C'était très bon"));
         commentList.add(new MyComments("Taco Bell","5/5","30/08","Meilleurs tacos que j'ai mangés"));
-        commentList.add(new MyComments("Quick","3/5","15/04","C'était très bon"));
+        commentList.add(new MyComments("Quick","3/5","15/04","C'était très bon. Le service est très agréable malgré que le serveur a renversé une bouteille de rouge sur ma fille."));
         commentList.add(new MyComments("Five guys","4/5","04/03","Très bon"));
         commentList.add(new MyComments("Subway","1/5","28/01","Pas bon du tout"));
     }
